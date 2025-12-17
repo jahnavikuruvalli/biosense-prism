104aa4360c61ee0e7c1055b375f611e10c1f426e
jahnavikuruvalli <jahnavikaranam2005@gmail.com>
2025-12-17 09:41:35 +0530

    FFT using Zig

---
+++ zig-ecg/src/main.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const fft = @import("fft.zig");
+
+pub fn main() !void {
+    var allocator = std.heap.page_allocator;
+
+    var signal = try allocator.alloc(std.math.Complex(f64), 8);
+    defer allocator.free(signal);
+
+    for (signal, 0..) |*v, i| {
+        v.* = .{ .re = @floatFromInt(i), .im = 0.0 };
+    }
+
+    fft.fft(signal);
+
+    for (signal) |v| {
+        std.debug.print("{d:.3} + {d:.3}i\n", .{ v.re, v.im });
+    }
+}
+
